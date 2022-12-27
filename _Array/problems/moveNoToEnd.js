bddcabd7fb9031fd5809d165b84eb132e7296300
Mohammed Shahul P.M <mshahulpm@gmail.com>
2022-12-27 22:31:12 +0530

    array

--- _Array/problems/moveNoToEnd.js
+++ _Array/problems/moveNoToEnd.js
@@ -25,7 +25,7 @@ function moveToEnd(array, num) {
 //     [6, 6, 6, 6, 6, 6, 6, 1, 1], 6
 // )
 
-
+// basic logic 
 function ArrayManipulation(n, queries) {
 
     let arr = new Array(n).fill(0)
@@ -39,8 +39,43 @@ function ArrayManipulation(n, queries) {
     console.log(Math.max(...arr))
 }
 
-ArrayManipulation(10, [
-    [1, 5, 3],
-    [4, 8, 7],
-    [6, 9, 1]
+
+// online solution 
+function ArrayManipulation2(n, queries) {
+
+    var max = 0;
+    var array = Array(n);
+
+    queries.forEach(q => {
+
+        // input is 1 based
+        var a = q[0] - 1;
+        var b = q[1];
+        var k = q[2]
+
+        array[a] = array[a] || 0;
+        array[a] = array[a] + k;
+
+        array[b] = array[b] || 0;
+        array[b] = array[b] - k;
+    })
+
+    var current = 0;
+    for (var i = 0; i < n; i++) {
+        current += (array[i] || 0);
+        if (current > max) {
+            max = current;
+        }
+    }
+    console.log(array)
+    console.log(max);
+}
+
+
+ArrayManipulation2(5, [
+    [1, 2, 100],
+    [2, 5, 100],
+    [2, 4, 100]
 ])
+
+// ArrayManipulation2()
